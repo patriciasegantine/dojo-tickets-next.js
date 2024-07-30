4ce0acbf7ddc506435f1b9a2a758bfbbe03a46ba
Patricia Segantine <pnsegantine@gmail.com>
2024-07-30 15:18:08 +0100

    feat: add active menu link

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+'use client'
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import { RouterEnum } from "../_enum/router-enum";
+import classNames from "classnames";
 
 export const Navbar = () => {
+  const pathname = usePathname();
+  
   return (
     <nav>
-      <Link href={RouterEnum.home} className='active'>Dashboard</Link>
-      <Link href={RouterEnum.tickets}>Tickets</Link>
+      <Link
+        href={RouterEnum.dashboard}
+        className={classNames({'active': pathname === RouterEnum.dashboard})}
+      >
+        Dashboard
+      </Link>
+      
+      <Link
+        href={RouterEnum.tickets}
+        className={classNames({'active': pathname === `/${RouterEnum.tickets}`})}
+      >
+        Tickets
+      </Link>
     </nav>
   );
 };
